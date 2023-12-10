502aa9cd12833980dc1f289bd30121dc50585bda
bonyoji <vdt.vincent.ta@gmail.com>
2023-12-10 15:28:36 +0900

    Add repeat function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num == 0 || string == '') {
+        return '';
+    } else if(num < 0) {
+        return 'ERROR';
+    } else {
+        let text = string;
+        for (let i = 1; i < num; i++) {
+            text += string;
+        }
+        return text;
+    }
 };
 
 // Do not edit below this line
